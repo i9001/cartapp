cfd63dd95e3dd7260421d149cdbb0182f02dc2dc
user1 <user1>
2022-01-04 12:57:45 -0600

    update

---
+++ source/contentscript.js
@@ -0,0 +1,105 @@
+/***********************************************************************
+  https://github.com/i9001
+  -------------------------------- (Copyright 2022) ---------------------------------
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER “AS IS” AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+    TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+    THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+    SUCH DAMAGE.
+ ***********************************************************************/
+ chrome.runtime.onMessage.addListener(function(e, t, n) {
+    if (e.cmd == "getPageInfo") {
+        n({
+            fromcontent: {
+                url: location.toString(),
+                name: document.getElementById("productTitle") ? document.getElementById("productTitle").innerText : "",
+                price: document.getElementById("priceblock_ourprice") ? document.getElementById("priceblock_ourprice").innerText : "",
+                img: document.getElementById("landingImage") ? document.getElementById("landingImage").src : ""
+            }
+        });
+    }
+    if (e.cmd == "gotoUrl") {
+        n("ok");
+        if (location.href == e.theUrl) location.reload(); else location.assign(e.theUrl);
+    }
+    if (e.cmd == "clearCart") {
+        n("ok");
+        i();
+    }
+});
+
+function i() {
+    var e = document.querySelectorAll("input");
+    e.forEach(function(e) {
+        if (e.getAttribute("data-action") == "delete") {
+            e.click();
+        }
+    });
+    setTimeout(i, 800);
+}
+
+function o() {
+    chrome.storage.sync.get([ "app_storage" ], function(e) {
+        if (e.app_storage == undefined) return;
+        var n = JSON.parse(e.app_storage);
+        if (n.state == "adding") {
+            setTimeout(function() {
+                console.log(n);
+                var e = document.getElementsByClassName("a-button-input")[0];
+                e.click();
+                n.adding_item_index++;
+                chrome.storage.sync.set({
+                    app_storage: JSON.stringify(n)
+                });
+                console.log(n.adding_item_index + 1, n.lists[n.adding_list_id].list_items.length);
+                if (n.adding_item_index + 1 > n.lists[n.adding_list_id].list_items.length) {
+                    n.adding_item_index = 0;
+                    n.adding_list_id = 0;
+                    n.state = "";
+                    console.log("adding over", n);
+                    chrome.storage.sync.set({
+                        app_storage: JSON.stringify(n)
+                    });
+                } else {
+                    var t = n.lists[n.adding_list_id].list_items[n.adding_item_index].url;
+                    setTimeout(function() {
+                        if (location.href == t) o(); else location.assign(t);
+                    }, 800);
+                }
+            }, 800);
+        }
+    });
+}
+
+o();
+
+var e = document.querySelectorAll('[data-a-strike="true"]');
+
+if (e.length > 0) {
+    var a = document.createElement("div");
+    a.innerText = "Sales found: ";
+    a.style.background = "white";
+    a.style.position = "static";
+    e.forEach(function(e) {
+        var t = e;
+        var n = document.createElement("a");
+        n.innerText = t.innerText;
+        n.onclick = function() {
+            t.scrollIntoView({
+                block: "center",
+                inline: "center"
+            });
+            t.style.background = "lightblue";
+            t.style.fontSize = "x-large";
+        };
+        a.appendChild(n);
+    });
+    document.body.prepend(a);
+}
---
+++ source/popup.js
@@ -0,0 +1,205 @@
+/***********************************************************************
+  https://github.com/i9001
+  -------------------------------- (Copyright 2022) ---------------------------------
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER “AS IS” AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+    TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+    THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+    SUCH DAMAGE.
+ ***********************************************************************/
+ var l = {
+    app_storage: {
+        lists: [],
+        state: "",
+        adding_item_index: 0,
+        adding_list_id: 0
+    }
+};
+
+document.getElementById("clearData").onclick = function() {
+    chrome.storage.sync.clear();
+};
+
+document.getElementById("clearCart").onclick = function() {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(e) {
+        chrome.tabs.sendMessage(e[0].id, {
+            url: location.href,
+            tabId: e[0].id,
+            cmd: "clearCart"
+        }, function(e) {});
+    });
+};
+
+function d(c) {
+    if (c === void 0) {
+        c = 0;
+    }
+    var p = document.getElementById("lists");
+    p.innerHTML = "";
+    chrome.storage.sync.get([ "app_storage" ], function(e) {
+        if (e.app_storage) l.app_storage = JSON.parse(e.app_storage);
+        l.app_storage.lists.forEach(function(t, i) {
+            c = 0;
+            var e = document.createElement("p");
+            e.innerText = i + ". " + t.name;
+            var a = document.createElement("a");
+            a.className = "btn";
+            a.innerText = "Edit List";
+            a.href = "#";
+            a.onclick = function() {
+                var e = prompt("List name", t.name);
+                if (e == null) return;
+                l.app_storage.lists[i].name = e;
+                chrome.storage.sync.set({
+                    app_storage: JSON.stringify(l.app_storage)
+                }, d);
+            };
+            e.appendChild(a);
+            var n = document.createElement("a");
+            n.className = "btn";
+            n.innerText = "Remove List";
+            n.href = "#";
+            n.onclick = function() {
+                l.app_storage.lists = g(l.app_storage.lists, i);
+                console.log(l.app_storage);
+                chrome.storage.sync.set({
+                    app_storage: JSON.stringify(l.app_storage)
+                }, d);
+            };
+            e.appendChild(n);
+            p.appendChild(e);
+            t.list_items.forEach(function(e, t) {
+                c += parseFloat(e.price.substr(1));
+                var a = document.createElement("div");
+                var n = document.createElement("img");
+                n.src = e.img;
+                n.width = 32;
+                n.style.verticalAlign = "middle";
+                a.appendChild(n);
+                var r = document.createElement("a");
+                r.innerText = t + ". " + e.name + " " + e.price;
+                r.href = "#";
+                r.onclick = function() {
+                    chrome.windows.create({
+                        url: e.url,
+                        incognito: true,
+                        type: "normal",
+                        focused: true
+                    });
+                };
+                a.appendChild(r);
+                var o = document.createElement("a");
+                o.className = "btn";
+                o.innerText = "x";
+                o.href = "#";
+                o.onclick = function() {
+                    l.app_storage.lists[i].list_items = g(l.app_storage.lists[i].list_items, t);
+                    chrome.storage.sync.set({
+                        app_storage: JSON.stringify(l.app_storage)
+                    }, function() {
+                        d();
+                    });
+                };
+                a.appendChild(o);
+                p.appendChild(a);
+            });
+            var r = document.createElement("button");
+            r.innerText = "Add to " + t.name;
+            r.onclick = function() {
+                chrome.tabs.query({
+                    active: true,
+                    currentWindow: true
+                }, function(e) {
+                    chrome.tabs.sendMessage(e[0].id, {
+                        url: location.href,
+                        tabId: e[0].id,
+                        cmd: "getPageInfo"
+                    }, function(e) {
+                        l.app_storage.lists[i].list_items.push(e.fromcontent);
+                        chrome.storage.sync.set({
+                            app_storage: JSON.stringify(l.app_storage)
+                        }, d);
+                    });
+                });
+            };
+            p.appendChild(r);
+            var o = document.createElement("button");
+            o.innerText = "Add all items to cart";
+            o.onclick = function() {
+                l.app_storage.state = "adding";
+                l.app_storage.adding_list_id = i;
+                l.app_storage.adding_item_index = 0;
+                var t = l.app_storage.lists[l.app_storage.adding_list_id].list_items[l.app_storage.adding_item_index].url;
+                console.log(1);
+                chrome.storage.sync.set({
+                    app_storage: JSON.stringify(l.app_storage)
+                }, function() {
+                    chrome.tabs.query({
+                        active: true,
+                        currentWindow: true
+                    }, function(e) {
+                        console.log(2, t);
+                        chrome.tabs.sendMessage(e[0].id, {
+                            theUrl: t,
+                            tabId: e[0].id,
+                            cmd: "gotoUrl"
+                        }, function(e) {});
+                    });
+                });
+            };
+            p.appendChild(o);
+            var s = document.createElement("span");
+            s.innerText = t.list_items.length + " items. total: " + c.toPrecision(2);
+            p.appendChild(s);
+        });
+    });
+}
+
+function e() {
+    chrome.storage.sync.get([ "app_storage" ], function(e) {
+        if (e.app_storage) l.app_storage = JSON.parse(e.app_storage);
+        var t = prompt("List name", "List");
+        if (t == null) return;
+        l.app_storage.lists.push({
+            name: t,
+            list_items: []
+        });
+        chrome.storage.sync.set({
+            app_storage: JSON.stringify(l.app_storage)
+        }, d);
+    });
+}
+
+document.getElementById("createList").onclick = function() {
+    e();
+};
+
+document.getElementById("reloadData").onclick = function() {
+    d();
+};
+
+document.getElementById("export").onclick = function() {
+    chrome.storage.sync.get([ "app_storage" ], function(e) {
+        if (e.app_storage) alert(e.app_storage);
+    });
+};
+
+d();
+
+function g(e, a) {
+    var n = [];
+    e.forEach(function(e, t) {
+        if (t != a) n.push(e);
+    });
+    return n;
+}
